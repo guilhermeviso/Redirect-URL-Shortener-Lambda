5e648c461e8ce18f8372eaa7d5b7e4325a0375c5
guilhermeviso <visoguilherme@gmail.com>
2024-11-22 20:40:28 -0300

    redirect url shortener

---
+++ src/main/java/org/example/UrlData.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+public class UrlData {
+    private String originalUrl;
+    private String expirationTime;
+}
